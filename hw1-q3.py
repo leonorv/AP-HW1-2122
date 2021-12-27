f6b9b0ad884a53b8505c67cd7145487f997676c5
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2021-12-27 10:32:53 +0000

    working on mlp

--- hw1-q3.py
+++ hw1-q3.py
@@ -11,6 +11,14 @@ import matplotlib.pyplot as plt
 
 import utils
 
+def relu(Z):
+    return np.maximum(0,Z)
+
+def drelu(dA, Z):
+    dZ = np.array(dA, copy = True)
+    dZ[Z <= 0] = 0;
+    return dZ;
+
 
 def configure_seed(seed):
     os.environ["PYTHONHASHSEED"] = str(seed)
@@ -54,7 +62,16 @@ class Perceptron(LinearModel):
         other arguments are ignored
         """
         # Q3.1a
-        raise NotImplementedError
+
+        # Sign function.
+        y_hat = np.argmax(self.W.dot(x_i))
+        if y_hat != y_i:
+            # Perceptron update.
+            self.W[y_i, :] += x_i
+            self.W[y_hat, :] -= x_i
+
+
+        #raise NotImplementedError
 
 
 class LogisticRegression(LinearModel):
@@ -65,21 +82,110 @@ class LogisticRegression(LinearModel):
         learning_rate (float): keep it at the default value for your plots
         """
         # Q3.1b
-        raise NotImplementedError
+        # Label scores according to the model (num_labels x 1).
+        label_scores = self.W.dot(x_i)[:, None]
+        # One-hot vector with the true label (num_labels x 1).
+        y_one_hot = np.zeros((np.size(self.W, 0), 1))
+        y_one_hot[y_i] = 1
+        # Softmax function.
+        # This gives the label probabilities according to the model (num_labels x 1).
+        label_probabilities = np.exp(label_scores) / np.sum(np.exp(label_scores))
+        # SGD update. W is num_labels x num_features.
+        self.W += learning_rate * (y_one_hot - label_probabilities) * x_i[None, :]
+        #raise NotImplementedError
 
 
 class MLP(object):
     # Q3.2b. This MLP skeleton code allows the MLP to be used in place of the
     # linear models with no changes to the training loop or evaluation code
     # in main().
-    def __init__(self, n_classes, n_features, hidden_size):
+    def __init__(self, n_classes, n_features, hidden_size, n_layers):
         # Initialize an MLP with a single hidden layer.
-        raise NotImplementedError
+        self.b1 = np.zeros(hidden_size)
+        self.b2 = np.zeros(n_classes)
+        self.w1 = np.random.normal(0.1, 0.1**2, size=(hidden_size, n_features))
+        self.w2 = np.random.normal(0.1, 0.1**2, size=(n_classes, hidden_size))
+
+        self.weights = [self.w1, self.w2]
+        self.biases = [self.b1, self.b2]
+        #raise NotImplementedError
+
+    def forward(self, x):
+        num_layers = 2
+        hiddens = []
+        for i in range(num_layers):
+            h = x if i == 0 else hiddens[i-1]
+            z = self.weights[i].dot(h) + self.biases[i]
+            if i < num_layers-1:  # Assume the output layer has no activation.
+                hiddens.append(relu(z))
+        output = z
+        # For classification this is a vector of logits (label scores).
+        # For regression this is a vector of predictions.
+        return output, hiddens
+
+    def compute_label_probabilities(self, output):
+        # softmax transformation.
+        #probs = np.exp(output) / np.sum(np.exp(output))
+
+        b = output.max()
+        y = np.exp(output - b)
+        return y / y.sum()
+        #return probs
+
+    def backward(self, x, y, output, hiddens):
+        num_layers = 2
+        z = output
+
+        probs = self.compute_label_probabilities(output)
+        grad_z = probs - y  # Grad of loss wrt last z.
+        grad_weights = []
+        grad_biases = []
+        for i in range(num_layers-1, -1, -1):
+            # Gradient of hidden parameters.
+            h = x if i == 0 else hiddens[i-1]
+            grad_weights.append(grad_z[:, None].dot(h[:, None].T))
+            grad_biases.append(grad_z)
+
+            # Gradient of hidden layer below.
+            grad_h = self.weights[i].T.dot(grad_z)
+
+            # Gradient of hidden layer below before activation.
+            #grad_z1 = grad_h1 * drelu(grad_h1, z1)   # Grad of loss wrt z3.
+            grad_z = grad_h * drelu(h, z[i])   # Grad of loss wrt z3.
+
+        grad_weights.reverse()
+        grad_biases.reverse()
+        return grad_weights, grad_biases
+
+    def update_parameters(self, grad_weights, grad_biases, eta):
+        num_layers = 2
+        for i in range(num_layers):
+            self.weights[i] -= eta*grad_weights[i]
+            self.biases[i] -= eta*grad_biases[i]
+
+    
+
+    def predict_label(self, output):
+        # The most probable label is also the label with the largest logit.
+        #y_hat = np.zeros_like(output)
+        #y_hat[np.argmax(output)] = 1
+        y_hat = np.argmax(output)
+        return y_hat
+
 
     def predict(self, X):
         # Compute the forward pass of the network. At prediction time, there is
         # no need to save the values of hidden nodes, whereas this is required
         # at training time.
+
+        predicted_labels = []
+        for x in X:
+            output, _ = self.forward(x)
+            y_hat = self.predict_label(output)
+            predicted_labels.append(y_hat)
+        predicted_labels = np.array(predicted_labels)
+        return predicted_labels
+
         raise NotImplementedError
 
     def evaluate(self, X, y):
@@ -89,12 +195,21 @@ class MLP(object):
         """
         # Identical to LinearModel.evaluate()
         y_hat = self.predict(X)
+        print(y_hat)
+        print(y)
         n_correct = (y == y_hat).sum()
         n_possible = y.shape[0]
         return n_correct / n_possible
 
     def train_epoch(self, X, y, learning_rate=0.001):
-        raise NotImplementedError
+
+        for x, y in zip(X, y):
+            output, hiddens = self.forward(x)
+            grad_weights, grad_biases = self.backward(x, y, output, hiddens)
+            self.update_parameters(grad_weights, grad_biases, eta=learning_rate)
+
+
+        #raise NotImplementedError
 
 
 def plot(epochs, valid_accs, test_accs):
